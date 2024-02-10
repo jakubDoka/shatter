5e3920641944634bf611e69bb0baba51e1ded087
mlokr <jdoka@crownsterling.io>
2024-02-11 00:42:57 +0100

    am dieing more

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sh ./install-css.sh
 sh ./install-script.sh
 sh ./install-ft-crypto.sh
 
-mkdir -p /bin
+mkdir -p ./bin
 cargo build --release --no-default-features
-cp ./target/release/sem bin
+cp ./target/release/sem ./bin/sem
 
